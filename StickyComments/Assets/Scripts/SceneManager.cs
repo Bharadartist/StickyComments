350d27e1ade1aa01d06942480b563ef9c32ce858
Bharadartist <ase.bharath@gmail.com>
2019-09-12 16:00:58 +0530

    Added touch to place functionaliy

---
+++ StickyComments/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject PlaceCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                Vector3 instantiatePosition = Camera.main.transform.position;
+                GameObject.Instantiate(PlaceCube, instantiatePosition, Quaternion.identity);
+            }
+        }
+    }
+}
